3033ca364ef2007eabef4eebdb3e5f3fcab7c97d
JUAN CARLOS <69373493+jcduro@users.noreply.github.com>
2026-02-01 16:58:23 -0500

    Update update_indicadores.php

--- update_indicadores.php
+++ update_indicadores.php
@@ -32,7 +32,7 @@ require_once __DIR__ . '/conexion.php'; // AJUSTA ESTA RUTA
 
   <!-- Font Awesome para iconos (si no lo tienes ya en el layout) -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
-   <link rel="stylesheet" href="css/chart.css"/>
+   <link rel="stylesheet" href="css/indicadores.css"/>
 
 
 </head>
